dbb0544251e74011151d7da1addf206edad6bbb6
fivetran-reneeli <renee.li@fivetran.com>
2021-11-22 16:45:52 -0600

    update yml and add int

---
+++ models/intermediate/int_tiktok_ads__most_recent_ad_group.sql
@@ -0,0 +1,22 @@
+with base as (
+
+    select *
+    from {{ var('ad_group_history') }}
+
+), row_num as (
+
+    select 
+        *,
+        row_number() over (partition by ad_group_id order by _fivetran_synced desc) as rn
+    from base
+
+), filtered as (
+
+    select *
+    from row_num
+    where rn = 1
+    
+)
+
+select *
+from filtered
