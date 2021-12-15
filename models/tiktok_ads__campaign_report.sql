8fb462ea6da60a256ae3d2fc01d343ea55b6a7ed
fivetran-reneeli <renee.li@fivetran.com>
2021-12-15 11:24:48 -0600

    add nullif

--- models/tiktok_ads__campaign_report.sql
+++ models/tiktok_ads__campaign_report.sql
@@ -25,9 +25,9 @@ with adapter as (
         sum(adapter.video_views_p_25) as video_views_p_25,
         sum(adapter.video_views_p_50) as video_views_p_50, 
         sum(adapter.video_views_p_75) as video_views_p_75,
-        round(sum(adapter.spend)/nullifzero(sum(adapter.clicks)),2) as daily_cpc,
-        round((sum(adapter.spend)/nullifzero(sum(adapter.impressions)))*1000,2) as daily_cpm,
-        round((sum(adapter.clicks)/nullifzero(sum(adapter.impressions)))*100,2) as daily_ctr
+        round(sum(adapter.spend)/nullif(sum(adapter.clicks),0),2) as daily_cpc,
+        round((sum(adapter.spend)/nullif(sum(adapter.impressions),0))*1000,2) as daily_cpm,
+        round((sum(adapter.clicks)/nullif(sum(adapter.impressions),0))*100,2) as daily_ctr
 
     from adapter
 
