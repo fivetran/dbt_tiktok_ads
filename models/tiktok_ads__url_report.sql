54c907155a8b618c39b24cdf4788466aa58e175e
fivetran-reneeli <renee.li@fivetran.com>
2022-07-19 17:51:24 +0200

    url report

---
+++ models/tiktok_ads__url_report.sql
@@ -0,0 +1,54 @@
+with hourly as (
+    
+    select *
+    from {{ var('campaign_report_hourly') }}
+
+), campaigns as (
+
+    select *
+    from {{ ref('int_tiktok_ads__most_recent_campaign') }}
+
+), advertiser as (
+
+    select *
+    from {{ var('advertiser') }}
+    
+), aggregated as (
+
+    select
+        cast(hourly.stat_time_hour as date) as date_day,
+        advertiser.advertiser_id,
+        advertiser.advertiser_name,
+        campaigns.campaign_id,
+        campaigns.campaign_name,
+        advertiser.currency,
+        sum(hourly.impressions) as impressions,
+        sum(hourly.clicks) as clicks,
+        sum(hourly.spend) as spend,
+        sum(hourly.reach) as reach,
+        sum(hourly.conversion) as conversion,
+        sum(hourly.likes) as likes,
+        sum(hourly.comments) as comments,
+        sum(hourly.shares) as shares,
+        sum(hourly.profile_visits) as profile_visits,
+        sum(hourly.follows) as follows,
+        sum(hourly.video_watched_2_s) as video_watched_2_s,
+        sum(hourly.video_watched_6_s) as video_watched_6_s,
+        sum(hourly.video_views_p_25) as video_views_p_25,
+        sum(hourly.video_views_p_50) as video_views_p_50, 
+        sum(hourly.video_views_p_75) as video_views_p_75,
+        sum(hourly.spend)/nullif(sum(hourly.clicks),0) as daily_cpc,
+        (sum(hourly.spend)/nullif(sum(hourly.impressions),0))*1000 as daily_cpm,
+        (sum(hourly.clicks)/nullif(sum(hourly.impressions),0))*100 as daily_ctr
+
+    from hourly
+    left join campaigns
+        on hourly.campaign_id = campaigns.campaign_id
+    left join advertiser
+        on campaigns.advertiser_id = advertiser.advertiser_id
+    {{ dbt_utils.group_by(6) }}
+
+)
+
+select *
+from aggregated
