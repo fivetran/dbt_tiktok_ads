42f332e533d20c28a75e612a8d6673ab4affd688
fivetran-reneeli <renee.li@fivetran.com>
2022-07-19 17:51:19 +0200

    campaign report

--- models/tiktok_ads__campaign_report.sql
+++ models/tiktok_ads__campaign_report.sql
@@ -1,37 +1,52 @@
-with adapter as (
+with hourly as (
+    
+    select *
+    from {{ var('campaign_report_hourly') }}
+
+), campaigns as (
 
     select *
-    from {{ ref('tiktok_ads__ad_adapter') }}
+    from {{ ref('int_tiktok_ads__most_recent_campaign') }}
+
+), advertiser as (
+
+    select *
+    from {{ var('advertiser') }}
     
 ), aggregated as (
 
     select
-        adapter.date_day,
-        adapter.campaign_id,
-        adapter.campaign_name,
-        adapter.advertiser_id,
-        sum(adapter.impressions) as impressions,
-        sum(adapter.clicks) as clicks,
-        sum(adapter.spend) as spend,
-        sum(adapter.reach) as reach,
-        sum(adapter.conversion) as conversion,
-        sum(adapter.likes) as likes,
-        sum(adapter.comments) as comments,
-        sum(adapter.shares) as shares,
-        sum(adapter.profile_visits) as profile_visits,
-        sum(adapter.follows) as follows,
-        sum(adapter.video_watched_2_s) as video_watched_2_s,
-        sum(adapter.video_watched_6_s) as video_watched_6_s,
-        sum(adapter.video_views_p_25) as video_views_p_25,
-        sum(adapter.video_views_p_50) as video_views_p_50, 
-        sum(adapter.video_views_p_75) as video_views_p_75,
-        sum(adapter.spend)/nullif(sum(adapter.clicks),0) as daily_cpc,
-        (sum(adapter.spend)/nullif(sum(adapter.impressions),0))*1000 as daily_cpm,
-        (sum(adapter.clicks)/nullif(sum(adapter.impressions),0))*100 as daily_ctr
+        cast(hourly.stat_time_hour as date) as date_day,
+        advertiser.advertiser_id,
+        advertiser.advertiser_name,
+        campaigns.campaign_id,
+        campaigns.campaign_name,
+        advertiser.currency,
+        sum(hourly.impressions) as impressions,
+        sum(hourly.clicks) as clicks,
+        sum(hourly.spend) as spend,
+        sum(hourly.reach) as reach,
+        sum(hourly.conversion) as conversion,
+        sum(hourly.likes) as likes,
+        sum(hourly.comments) as comments,
+        sum(hourly.shares) as shares,
+        sum(hourly.profile_visits) as profile_visits,
+        sum(hourly.follows) as follows,
+        sum(hourly.video_watched_2_s) as video_watched_2_s,
+        sum(hourly.video_watched_6_s) as video_watched_6_s,
+        sum(hourly.video_views_p_25) as video_views_p_25,
+        sum(hourly.video_views_p_50) as video_views_p_50, 
+        sum(hourly.video_views_p_75) as video_views_p_75,
+        sum(hourly.spend)/nullif(sum(hourly.clicks),0) as daily_cpc,
+        (sum(hourly.spend)/nullif(sum(hourly.impressions),0))*1000 as daily_cpm,
+        (sum(hourly.clicks)/nullif(sum(hourly.impressions),0))*100 as daily_ctr
 
-    from adapter
-
-    {{ dbt_utils.group_by(4) }}
+    from hourly
+    left join campaigns
+        on hourly.campaign_id = campaigns.campaign_id
+    left join advertiser
+        on campaigns.advertiser_id = advertiser.advertiser_id
+    {{ dbt_utils.group_by(6) }}
 
 )
 
